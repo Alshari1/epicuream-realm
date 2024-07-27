04bc636f8d9bbafb2a9b7200e7ddfed245ecf457
Alshari1 <securetyalshare@gmai.com>
2024-07-27 10:20:54 +0600

    banner and navbar added

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../components/Main/Main";
+import Home from "../components/Home/Home/Home";
+import Details from "../components/Details/Details";
+import ChefDetails from "../components/ChefDetails/ChefDetails";
+
+export  const router = createBrowserRouter([
+    {
+        path:'/',
+        element:<Main></Main>,
+        children:[
+            {
+                path:'/',
+                element:<Home></Home>
+            }
+        ]
+    },
+    {
+        path:'/details',
+        element:<Details></Details>,
+        children:[
+            {
+                path:'/details',
+                element:<ChefDetails></ChefDetails>           
+            }
+        ]
+    }
+  ])
---
+++ src/components/ChefDetails/ChefDetails.jsx
@@ -0,0 +1,9 @@
+const ChefDetails = () => {
+    return (
+        <div>
+            <h1>from chef details</h1>
+        </div>
+    );
+};
+
+export default ChefDetails;
