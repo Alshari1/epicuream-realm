82b08dcf8376d55860cd27943aae5912e11d6aa0
Alshari1 <securetyalshare@gmai.com>
2024-07-27 10:41:25 +0600

    jaheda modified files again

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './Routes/Routes.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const App = () => {
+  return (
+    <div>
+      Hello, World!
+    </div>
+  );
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}></RouterProvider>
-  </React.StrictMode>,
-)
+    <App />
+  </React.StrictMode>
+);
